7372f701c93cb84d40d64a11e19e639d5afdb4e0
Santiago <sanbemal@gmail.com>
2023-07-21 08:38:38 +0200

    35. Implementando inyección de dependencia con la anotación @Autowired

--- src/main/java/controllers/IndexController.java
+++ src/main/java/controllers/IndexController.java
@@ -1,18 +1,19 @@
 package controllers;
 
 import models.services.MiServicio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-
+    @Autowired
     private MiServicio servicio = new MiServicio();
 
     @GetMapping({"/", "", "/index"})
     public String index(Model model) {
-       model.addAttribute("objeto", servicio.operacion());
+        model.addAttribute("objeto", servicio.operacion());
         return "index";
     }
 }
--- src/main/java/models/services/MiServicio.java
+++ src/main/java/models/services/MiServicio.java
@@ -1,5 +1,8 @@
 package models.services;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MiServicio {
     public String operacion(){
         return "ejecutando algún proceso importante...";
