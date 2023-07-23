60359ae3e1fa6d4629064279a0b9c55a9ef3ccd3
Santiago <sanbemal@gmail.com>
2023-07-23 20:55:00 +0200

    41. Ejemplo Factura y relaciones entre sus componentes

---
+++ src/main/java/models/domain/Cliente.java
@@ -0,0 +1,28 @@
+package models.domain;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cliente {
+    @Value("${cliente.nombre")
+    private String nombre;
+    @Value("${cliente.apellido")
+    private String Apellido;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String apellido) {
+        Apellido = apellido;
+    }
+}
